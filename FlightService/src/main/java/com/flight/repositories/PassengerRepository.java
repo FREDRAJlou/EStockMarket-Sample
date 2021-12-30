c77d7d0da06b92c1d2a5c66c83618a07ff57b13b
Fredrick <fedrick.lourduraj@macys.com>
2021-12-30 20:40:11 +0530

    Initial Commit

---
+++ FlightService/src/main/java/com/flight/repositories/PassengerRepository.java
@@ -0,0 +1,10 @@
+package com.flight.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//import com.flight.entities.Passenger;
+//
+//public interface PassengerRepository extends JpaRepository<Passenger, Integer>{
+//
+//
+//}
